562034e275f7b49e28837d435371db507c9df58b
DariaHoncharenko <honcharenko.daria.v@gmail.com>
2021-11-07 15:15:15 +0200

    implementation Lecture 2

--- lib/hash_calculator/compute_heavy_task_performer.dart
+++ lib/hash_calculator/compute_heavy_task_performer.dart
@@ -1,8 +1,20 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:lecture_2_hometask_starter/hash_calculator/heavy_task_performer.dart';
+import 'package:lecture_2_hometask_starter/helpers/random_number_hash_calculator.dart';
+
+import '../constants.dart';
 
 class ComputeHeavyTaskPerformer implements HeavyTaskPerformer {
+
+  static Future<String> _randomHashNumber(int n) async {
+    return RandomNumberHashCalculator().calculateRandomNumberHash(iterationsCount: n);
+  }
+  
   @override
-  Future<String>doSomeHeavyWork() {
-    throw UnimplementedError();
+  Future<String> doSomeHeavyWork() async {
+    return compute<int, String>(_randomHashNumber, DefaultIterationsCount);
+
   }
 }
--- lib/hash_calculator/spawned_isolate_task_performer.dart
+++ lib/hash_calculator/spawned_isolate_task_performer.dart
@@ -2,14 +2,55 @@ import 'dart:async';
 import 'dart:isolate';
 
 import 'package:lecture_2_hometask_starter/hash_calculator/heavy_task_performer.dart';
+import 'package:lecture_2_hometask_starter/helpers/random_number_hash_calculator.dart';
+
 
 class SpawnedIsolateTaskPerformer implements HeavyTaskPerformer {
 
-  Isolate? _isolate;
-  Completer? _completer;
+    Isolate? isolate;
 
-  @override
-  Future<String> doSomeHeavyWork() {
-    throw UnimplementedError();
-  }
+    final _completer = Completer<String>();
+
+    static String _randomHashNumber(int n) {
+      return RandomNumberHashCalculator().calculateRandomNumberHash(iterationsCount: n);
+    }
+    
+    @override
+    Future<String> doSomeHeavyWork() async {
+
+      try {
+        final spawnerReceivePort = ReceivePort();
+        isolate = await Isolate.spawn(
+          _establishCommunicationWithSpawner,
+          spawnerReceivePort.sendPort,
+        );
+
+        spawnerReceivePort.listen((message) {
+          if (message is SendPort) {
+            message.send(_randomHashNumber);
+          } else if (message is String) {
+            _completer.complete(message);
+          }
+        });
+      } catch (e) {
+        _completer.completeError(e);
+      }
+
+      return _completer.future;
+    }
+
+
+    static void _establishCommunicationWithSpawner(SendPort spawnerSendPort) {
+      final ReceivePort spawneeReceivePort = ReceivePort();
+      spawnerSendPort.send(spawneeReceivePort.sendPort);
+
+      spawneeReceivePort.listen((message) {
+        if (message is int) {
+          final result = _randomHashNumber(message);
+          spawnerSendPort.send(result);
+
+          spawneeReceivePort.close();
+        }
+      });
+    }
 }
